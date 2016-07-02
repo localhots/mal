dcd75825080c83c090d8ea6222e31432a82a92d6
Gregory Eremin <g@erem.in>
2016-07-02 17:23:25 +0200

    By the book

---
+++ scala/step0_repl.scala
@@ -0,0 +1,38 @@
+object step0_repl {
+    val prompt = "user> "
+
+    def main(args: Array[String]) = {
+        repl()
+    }
+
+    def repl() = {
+        var continue = true
+        while (continue) {
+            print(prompt)
+
+            val line = readLine()
+            if (line == null) {
+                continue = false
+            } else {
+                rep(line)
+            }
+        }
+    }
+
+    def rep(str: String) = {
+        PRINT(EVAL(READ(str)))
+    }
+
+
+    def READ(str: String) : String = {
+        str
+    }
+
+    def EVAL(str: String) : String = {
+        str
+    }
+
+    def PRINT(result: String) = {
+        println(result)
+    }
+}
